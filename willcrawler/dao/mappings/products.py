6a496a6838a4530fd2bb7732b888f4f7383c3107
Will Roger Pereira <willrogerpereira@hotmail.com>
2019-09-28 16:45:11 +0900

    Fields required

--- willcrawler/dao/mappings/products.py
+++ willcrawler/dao/mappings/products.py
@@ -2,26 +2,26 @@ from elasticsearch_dsl import DocType, Text, Keyword, Nested, Float
 
 
 class Products(DocType):
-    about = Text(fields={"keyword": Keyword()})
-    brand = Text(fields={"keyword": Keyword()})
-    care = Text(fields={"keyword": Keyword()})
-    code = Text(fields={"keyword": Keyword()})
-    details = Text(fields={"keyword": Keyword()})
-    gender = Text(fields={"keyword": Keyword()})
-    images = Text(fields={"keyword": Keyword()})
-    kind = Text(fields={"keyword": Keyword()})
-    link = Text(fields={"keyword": Keyword()})
-    name = Text(fields={"keyword": Keyword()})
+    about = Text(fields={"keyword": Keyword()}, required=True)
+    brand = Text(fields={"keyword": Keyword()}, required=True)
+    care = Text(fields={"keyword": Keyword()}, required=True)
+    code = Text(fields={"keyword": Keyword()}, required=True)
+    details = Text(fields={"keyword": Keyword()}, required=True)
+    gender = Text(fields={"keyword": Keyword()}, required=True)
+    images = Text(fields={"keyword": Keyword()}, required=True)
+    kind = Text(fields={"keyword": Keyword()}, required=True)
+    link = Text(fields={"keyword": Keyword()}, required=True)
+    name = Text(fields={"keyword": Keyword()}, required=True)
     price = Nested(
         properties={
-            "currency": Text(fields={'keyword': Keyword()}),
-            "outlet": Float(),
-            "retail": Float()
+            "currency": Text(fields={'keyword': Keyword()}, required=True),
+            "outlet": Float(required=True),
+            "retail": Float(required=True)
         }
     )
-    sessionid = Text(fields={"keyword": Keyword()})
-    sessionname = Text(fields={"keyword": Keyword()})
-    storename = Text(fields={"keyword": Keyword()})
+    sessionid = Text(fields={"keyword": Keyword()}, required=True)
+    sessionname = Text(fields={"keyword": Keyword()}, required=True)
+    storename = Text(fields={"keyword": Keyword()}, required=True)
 
     class Meta:
         index = "store"
